2994026727e0573723c0e4bc135534d0ad7bca40
anjali <anjalicg2001@gmail.com>
2023-10-30 12:08:22 -0700

    Sierpinski Triangle

---
+++ euler/example/ex_sum_of_multiples_of_3_or_5.py
@@ -0,0 +1,5 @@
+from lib.return_sum import return_sum
+if __file__=="__main__":
+    print(return_sum(1000))
+
+
---
+++ euler/lib/tests/test_sum_of_multiples_of_3_or_5.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+
+
+def test_basic():
+    assert return_sum(10)==23
+
+def test_q():
+    print(return_sum(1000))
+
